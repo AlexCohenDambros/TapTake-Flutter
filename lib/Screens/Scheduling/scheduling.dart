1fd06f9ad8180e264210af8eec93e00dd061835d
alex dambros <alex-dambros@hotmail.com>
2022-07-09 02:39:16 -0300

    added time picker

--- lib/Screens/Scheduling/scheduling.dart
+++ lib/Screens/Scheduling/scheduling.dart
@@ -15,6 +15,8 @@ class Scheduling extends StatefulWidget {
 }
 
 class _SchedulingStateState extends State<Scheduling> {
+  TimeOfDay? time = TimeOfDay.now();
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -25,11 +27,10 @@ class _SchedulingStateState extends State<Scheduling> {
       ),
       body: SingleChildScrollView(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          SizedBox(height: size.height * 0.02, child: Container()),
           FlutterCalendar(
               selectionMode: CalendarSelectionMode.single,
               minDate: DateTime.now().subtract(const Duration(days: 0)),
-              maxDate: DateTime.utc(2030, 3, 14),
+              maxDate: DateTime.now().subtract(const Duration(days: 30)),
               focusedDate: DateTime.now(),
               locale: "pt_BR",
               textStyle: const CalendarTextStyle(
@@ -49,6 +50,38 @@ class _SchedulingStateState extends State<Scheduling> {
                     color: Colors.white,
                   )),
               onDayPressed: (DateTime date) {}),
+          SizedBox(height: size.height * 0.05),
+          const Text(
+            "Selecione um horário: ",
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: size.height * 0.02),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () async {
+                  TimeOfDay? newtime = await showTimePicker(
+                    context: context,
+                    initialTime: time!,
+                  );
+                  if (newtime != null) {
+                    setState(() {
+                      time = newtime;
+                    });
+                  }
+                },
+                child: Text(
+                  "${time!.hour.toString()}:${time!.minute.toString()}",
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
           SizedBox(height: size.height * 0.05),
           RoundedButton(
             text: "Próximo",
